d58fd930bd6bf4b71c48fc812e8ac0e60cc5af50
AdornChoga <adornchoga@gmail.com>
2022-04-03 23:58:40 +0200

    Create Teacher class

--- student.rb
+++ student.rb
@@ -1,8 +1,8 @@
 require_relative 'person'
 
 class Student < Person
-  def initialize(age, name, classroom)
-    super(age, name)
+  def initialize(classroom)
+    super
     @classroom = classroom
   end
 
