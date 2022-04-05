ab06ff7e8d553a933b1c39b937788f76eb0c3fdb
AdornChoga <adornchoga@gmail.com>
2022-04-05 17:52:50 +0200

    Put decorators in a separate file in a folder

--- person.rb
+++ person.rb
@@ -1,32 +1,4 @@
-class Nameable
-  def correct_name
-    raise NotImplementedError, "#{self.class} has not implemented method '#{__method__}'"
-  end
-end
-
-class BaseDecorator < Nameable
-  def initialize(nameable)
-    super()
-    @nameable = nameable
-  end
-
-  def correct_name
-    @nameable.correct_name
-  end
-end
-
-class CapitalizeDecorator < BaseDecorator
-  def correct_name
-    @nameable.correct_name.capitalize
-  end
-end
-
-class TrimmerDecorator < BaseDecorator
-  def correct_name
-    str = @nameable.correct_name
-    str[0...10] if str.length >= 10
-  end
-end
+require './decorators/person/correct_name'
 
 class Person < Nameable
   attr_accessor :name, :age
@@ -53,10 +25,3 @@ class Person < Nameable
     @age >= 18
   end
 end
-
-person = Person.new(22, 'maximilianus')
-puts person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
