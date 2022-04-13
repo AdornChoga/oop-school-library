eaaa195e181aabb092256ce7a40651f46b2e5805
AdornChoga <adornchoga@gmail.com>
2022-04-13 07:30:48 +0200

    Moved input requests and validations to main.rb

--- app_funcs/app_books.rb
+++ app_funcs/app_books.rb
@@ -1,16 +1,13 @@
 require './book'
-require_relative 'get_user_input'
 
-module Books
-  include UserInput
+module AppBooks
   def list_all_books(books)
     puts 'No book added yet' if books.empty?
     books.map { |book| puts "Title: \"#{book.title}\", Author: #{book.author}" }
   end
 
-  def create_book(books)
-    inputs = user_input(%w[Title Author])
-    book = Book.new(inputs[0], inputs[1])
+  def create_book(title, author, books)
+    book = Book.new(title, author)
     books << book
     puts 'Book created successfully'
   end
